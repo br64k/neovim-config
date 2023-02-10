9c228f97091a89ce1606b3d124c019a433c71e66
Roman Brodsky <brodsky.roma@gmail.com>
2023-02-10 22:09:16 +0200

    refactor

--- lua/user/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-local colorscheme = "carbonfox"
-
-local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found.")
-  return
-end
