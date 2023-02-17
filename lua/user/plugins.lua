b6d78e6d5de680274c31dbeda630583ef3e84560
Roman Brodsky <brodsky.roma@gmail.com>
2023-02-17 18:12:14 +0200

    format

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -39,22 +39,25 @@ packer.init {
 }
 
 return packer.startup(function(use)
-  use { "folke/lazy.nvim", tag = "stable" }
-  use 'lewis6991/impatient.nvim'
-  use "wbthomason/packer.nvim"
-  use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim"
-  use "github/copilot.vim"
-  use "theprimeagen/harpoon"
-  use "nvim-telescope/telescope.nvim"
-  use "mbbill/undotree"
-  use "tpope/vim-fugitive"
-  use "tpope/vim-surround"
   use {
-    'goolord/alpha-nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' },
+    "folke/lazy.nvim",
+    tag = "stable"
+  }
+  use { "lewis6991/impatient.nvim" }
+  use { "wbthomason/packer.nvim" }
+  use { "nvim-lua/popup.nvim" }
+  use { "nvim-lua/plenary.nvim" }
+  use { "github/copilot.vim" }
+  use { "theprimeagen/harpoon" }
+  use { "nvim-telescope/telescope.nvim" }
+  use { "mbbill/undotree" }
+  use { "tpope/vim-fugitive" }
+  use { "tpope/vim-surround" }
+  use {
+    "goolord/alpha-nvim",
+    requires = { "nvim-tree/nvim-web-devicons" },
     config = function ()
-      require'alpha'.setup(require'alpha.themes.dashboard'.config)
+      require("alpha").setup(require"alpha.themes.dashboard".config)
     end
   }
   use {
@@ -67,38 +70,38 @@ return packer.startup(function(use)
     end
   }
   use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
     requires = {
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      {"neovim/nvim-lspconfig"},
+      {"williamboman/mason.nvim"},
+      {"williamboman/mason-lspconfig.nvim"},
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lua'},
+      {"hrsh7th/nvim-cmp"},
+      {"hrsh7th/cmp-nvim-lsp"},
+      {"hrsh7th/cmp-buffer"},
+      {"hrsh7th/cmp-path"},
+      {"saadparwaiz1/cmp_luasnip"},
+      {"hrsh7th/cmp-nvim-lua"},
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      {"L3MON4D3/LuaSnip"},
+      {"rafamadriz/friendly-snippets"},
     }
   }
 
   use {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
       ts_update()
     end,
   }
   use {
-    'nvim-tree/nvim-tree.lua',
+    "nvim-tree/nvim-tree.lua",
     requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      "nvim-tree/nvim-web-devicons", -- optional, for file icons
     },
   }
   use {
